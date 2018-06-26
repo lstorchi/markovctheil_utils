372d36c3907e545f0f0fd34fcb0faddc9e0ee954
Loriano Storchi <loriano@storchi.org>
2018-06-26 16:48:00 +0200

    addede basi code

---
+++ unify.py
@@ -0,0 +1,51 @@
+import sys
+
+def read_from_file (filename):
+    dates = []
+    values = []
+
+    fp = open(filename)
+    fp.readline()
+    for l in fp:
+        line = l.replace("\"", "")
+        sline = line.split(",")
+        
+        if len(sline) < 7:
+            print "Error in file format ", len(sline)
+            print sline
+            print l
+            exit(1)
+    
+        datasplit = sline[0].split()
+        if len(datasplit) != 2:
+            print "Error in file format "
+            print l
+            print datasplit
+            exit(1)
+        
+        day = int(datasplit[1])
+        month = datasplit[0]
+    
+        year = int(sline[1])
+        data = float(sline[2])
+    
+        dates.append(str(day)+"_"+month+"_"+str(year))
+        values.append(data)
+    
+    fp.close()
+
+    return dates, values
+
+#####################################################################
+
+
+if len(sys.argv) != 3:
+    print "usage: ", sys.argv[0], " file1 file2"
+    exit(1)
+
+fileorig = sys.argv[1]
+file = sys.argv[2]
+
+refdates, refvalues = read_from_file (fileorig)
+dates, values = read_from_file (file)
+
