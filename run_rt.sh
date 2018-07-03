8231ac37bfae91e59989be48de243fc97fc0ae7b
Loriano Storchi <loriano@storchi.org>
2018-07-03 10:12:25 +0200

    added script and ratings

---
+++ run_rt.sh
@@ -0,0 +1,15 @@
+for name in Germany Austria Belgium Bulgaria Croatia Czech_Republic Denmark Finland France Greece Hungary Ireland Italy Lithuania Malta Netherlands Poland Romania Slovakia Slovenia Spain Sweden United_Kingdom Portugal
+do 
+   ./rating_step1.sh ./"$name".csv 
+
+   python rating_step2.py ./"$name"_moodys.csv > ./"$name"_moodys_ref.csv
+   python rating_step2.py ./"$name"_sep.csv > ./"$name"_sep_ref.csv
+   python rating_step2.py ./"$name"_fitch.csv > ./"$name"_fitch_ref.csv
+   
+   python rating_step3.py ./dates.csv ./"$name"_moodys_ref.csv > out.csv 
+   mv out.csv ./"$name"_moodys_ref.csv
+   python rating_step3.py ./dates.csv ./"$name"_sep_ref.csv > out.csv 
+   mv out.csv ./"$name"_sep_ref.csv
+   python rating_step3.py ./dates.csv ./"$name"_fitch_ref.csv > out.csv 
+   mv out.csv ./"$name"_fitch_ref.csv
+done
